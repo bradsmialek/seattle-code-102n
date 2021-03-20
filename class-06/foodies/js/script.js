0effaf0865caf30e9ef64b8d88b51c2d18d344c5
acanady <alfonso.canadyjr@gmail.com>
2021-03-20 15:47:26 -0500

    Add files via upload

---
+++ class-06/foodies/js/script.js
@@ -0,0 +1,32 @@
+/*
+Arithmetic expressions
+let x = 7;
+let x;
+x = 7;
+x + 7;
+x - 7;
+x / 7;
+2 * 7;
+x++; x--;
+3%2 */
+
+//A conditional expression has either of two values based 
+//on a condition true or false
+let b;
+let a;
+a == b;
+a != b;
+a < b;
+a > b;
+a <= b;
+a >= b;
+
+let age = prompt('How old are you?');
+
+let color = prompt('Give me a color');
+
+document.getElementById("pastblogs").style.color = color;
+
+if(age < 13){
+    alert('You are too young!');
+}
